cf1669815ee693915ad3fd270d5ab3b1ec7fe7de
John Michael Paraiso <johnmparaiso@gmail.com>
2020-05-24 13:15:37 +0100

    added waypoints system

---
+++ Assets/Scripts/splashScreen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class splashScreen : MonoBehaviour
+{
+    void Start()
+    {
+        Invoke("LoadScene", 5);
+    }
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
